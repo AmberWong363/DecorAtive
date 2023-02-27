f727de9cfb7846e449cb3bc4fe0864bb6378a48f
Amber Wong <ambwong363@gmail.com>
2023-02-27 13:37:37 -0500

    Initial Commit

---
+++ Decorative/ContentView.swift
@@ -0,0 +1,16 @@
+//
+//  ContentView.swift
+//  Decorative
+//
+//  Created by Amber Wong (student LM) on 2/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
