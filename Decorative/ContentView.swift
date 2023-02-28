baf9708bbcdbf86a51fac0e35c91c180364b4085
Maya Taylor <s016719@students.lmsd.org>
2023-02-28 14:18:33 -0500

    Added VStack

--- Decorative/ContentView.swift
+++ Decorative/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-            
+        VStack {
+            Text("Hello, world!")
+                .padding()
+        }
+        
     }
 }
 
