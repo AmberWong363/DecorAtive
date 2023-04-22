c209814920268ee61a1aa35e62ecd19d6e05fe45
Reuben <s021632@students.lmsd.org>
2023-04-22 17:24:36 -0400

    yuh

---
+++ Decorative/Model/Root.swift
@@ -0,0 +1,16 @@
+//
+//  Root.swift
+//  Decorative
+import Foundation
+class Root : ObservableObject {
+    @Published var rootFolder : Folder
+    @Published var folder : Folder
+    @Published var previous : [Folder]
+    @Published var savedID : UUID = UUID()
+    
+    init(_ folder : Folder = Folder("root", files: [], folders: [Folder("foot", files: [], folders: [Folder(), Folder(), Folder()]), Folder(), Folder()]), rootFolder : Folder = Folder(), previous : [Folder] = [] ) {
+        self.folder = folder
+        self.rootFolder = rootFolder
+        self.previous = previous
+    }
+}
