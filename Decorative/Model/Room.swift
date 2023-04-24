27c87bbff1144081d06d28f7055c24846cf175e1
Reuben <s021632@students.lmsd.org>
2023-04-24 07:51:24 -0400

    a

--- Decorative/Model/Room.swift
+++ Decorative/Model/Room.swift
@@ -24,28 +24,28 @@ class Room : ObservableObject {
         for i in 0..<room.count {
             for j in 0..<room[i].count {
                 // Top
-                if i == 0 && room[i][j].selected {
+                if i == 0 && room[i][j].selected && room[i][j].wallType[3] == .none {
                     room[i][j].wallType[3] = .wall
-                } else if room[i][j].selected && !room[i-1][j].selected {
+                } else if i != 0 && room[i][j].selected && !room[i-1][j].selected && room[i][j].wallType[3] == .none {
                     room[i][j].wallType[3] = .wall
                 }
                 
                 // Bottom
-                if i == room.count - 1 && room[i][j].selected {
+                if i == room.count - 1 && room[i][j].selected && room[i][j].wallType[2] == .none {
                     room[i][j].wallType[2] = .wall
-                } else if room[i][j].selected && !room[i+1][j].selected {
+                } else if i != room.count - 1 && room[i][j].selected && !room[i+1][j].selected && room[i][j].wallType[2] == .none {
                     room[i][j].wallType[2] = .wall
                 }
                 // Trailing
-                if j == 0 && room[i][j].selected {
+                if j == 0 && room[i][j].selected && room[i][j].wallType[0] == .none {
                     room[i][j].wallType[0] = .wall
-                } else if room[i][j].selected && !room[i][j-1].selected {
+                } else if j != 0 && room[i][j].selected && !room[i][j-1].selected && room[i][j].wallType[0] == .none {
                     room[i][j].wallType[0] = .wall
                 }
                 // Leading
-                if j == room[i].count - 1 && room[i][j].selected {
+                if j == room[i].count - 1 && room[i][j].selected && room[i][j].wallType[1] == .none {
                     room[i][j].wallType[1] = .wall
-                } else if room[i][j].selected && !room[i][j+1].selected {
+                } else if j != room[i].count - 1 && room[i][j].selected && !room[i][j+1].selected && room[i][j].wallType[1] == .none {
                     room[i][j].wallType[1] = .wall
                 }
             }
